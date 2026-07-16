3f68a12ea3b365d1076e23b8530d60ece99ec923
Ya-cien <velardey55@gmail.com>
2026-07-15 18:56:20 -0700

    Actividad 3 terminada

---
+++ 23400631_Velarde_Virgen_Yamil_Adrian/U4/ejercicios/Actividad3/Act3.js
@@ -0,0 +1,128 @@
+
+/*Ejercicio 1*/
+function potencia(base, exponente) {
+
+    var resultado = 0
+    for (var i = 0; i <= exponente; i++) {
+        resultado += base;
+    }
+    console.log(base, "elevado a", exponente, "es", resultado)
+
+}
+
+potencia(2, 3);
+/*Ejercicio 2*/
+function cajero() {
+    var saldo = 5000;
+    do {
+        console.log("1. Consultar saldo \n" +
+            "2. Depositar\n" +
+            "3. Retirar\n" +
+            "4. Salir"
+        )
+        var opcion = Number(prompt("Ingresa una opción"))
+        switch (opcion) {
+            case (1):
+                console.log("$" + saldo)
+                break;
+            case (2):
+                var deposito = Number(prompt("Ingrese el monto a depositar"));
+                saldo += deposito;
+                console.log("Operación realizada con éxito")
+                break;
+            case (3):
+                var retiro = Number(prompt("Ingrese la cantidad a retirar"))
+                if (retiro > saldo) {
+                    console.log("Operación no realizadan\n" + "La cantidad a retirar es mayor a saldo")
+                } else {
+                    saldo -= retiro;
+                    console.log("Operación realizada con éxito")
+                }
+                break
+            case (4):
+                console.log("SALIENDO")
+                break;
+        }
+    } while (opcion != 4);
+
+}
+cajero();
+/*Ejercicio 3*/
+function dados() {
+    var contador = 0;
+    do {
+        var n1 = Math.floor(Math.random() * 6) + 1;
+        var n2 = Math.floor(Math.random() * 6) + 1;
+        console.log(n1 + "-" + n2)
+        contador++;
+    } while (n1 != n2)
+
+    console.log("Se necesitaron", contador, "lanzamientos")
+}
+dados();
+/*Ejercicio 4*/
+function adivinar() {
+    var n1 = Math.floor(Math.random() * 100) + 1;
+    console.log(n1);
+
+    for (var i = 0; i < 7; i++) {
+        var intento = Number(prompt("Adivina el número"));
+        if (intento < n1) {
+            console.log("Intenta mas arriba")
+        } else if (intento > n1) {
+            console.log("Intenta mas abajo")
+        } else {
+            console.log("Lo encontraste")
+            break;
+        }
+    }
+
+}
+adivinar();
+
+/*Ejercicio 5*/
+function ventas() {
+
+
+    var cantidadV = Number(prompt("Ingresa la cantidad de ventas a registar"))
+    var totalIngresos = 0;
+    var unidadesXProducto;
+    var vtas = [];
+
+    for (var i = 0; i < cantidadV; i++) {
+        console.log("Venta", i + 1)
+        var nombreV = prompt("Ingresa el nombre del vendedor");
+        var nombreP = prompt("Ingresa el nombre del producto");
+        var cVendida = prompt("Ingresa la cantidad vendida");
+        var pUnitario = prompt("Ingresa el precio unitario");
+        console.log("Vendiste $" + cVendida * pUnitario);
+
+        var venta = {
+            nombreVendedor: nombreV,
+            nombreProducto: nombreP,
+            cantidadVendida: cVendida,
+            precioUnitario: pUnitario
+        }
+        vtas.push(venta)
+        totalIngresos += cVendida * pUnitario;
+    }
+    console.log("===================================")
+    console.log("Total de ventas realizadas:", cantidadV);
+    console.log("Total de ingresos: $" + totalIngresos);
+    console.log("Unidades vendidas por producto:")
+    vtas.forEach(vta => {
+        console.log("* " + vta.nombreProducto + ":", vta.cantidadVendida)
+    });
+    var mayor = 0;
+    var vendedor = " ";
+    vtas.forEach(vta => {
+        if ((vta.cantidadVendida * vta.precioUnitario) > mayor) {
+            mayor = vta.cantidadVendida * vta.precioUnitario;
+            vendedor = vta.nombreVendedor;
+        }
+    })
+    console.log("Vendedor con mas ventas:", vendedor, "($" + mayor + ")");
+}
+
+
+
--- 23400631_Velarde_Virgen_Yamil_Adrian/U4/ejercicios/index.js
+++ 23400631_Velarde_Virgen_Yamil_Adrian/U4/ejercicios/index.js
@@ -92,9 +92,141 @@ numeros.forEach(numero => {
     console.log(numero)
 });
 */
-
+/*
 let numeros = [1,2,3,4,5]
 let dobles = numeros.map(numero => {
     return numero*2;
 });
 console.log(dobles);
+
+console.log(typeof(10))
+console.log(typeof("Hola"))
+console.log(typeof(true))
+console.log(typeof({}))
+
+const edad = 17;
+const mensaje = edad >= 18 ? "Mayor de edad" : "Menor de edad"
+console.log(mensaje)
+
+//
+const estudiante = {
+   nombre: "Pedro"
+}
+console.log(estudiante.direccion?.calle)
+// No marca error
+
+//
+const estudiante = {
+   nombre: "Pedro",
+   direccion: {
+      calle: "calle"
+   }
+}
+
+console.log(estudiante.edad ?? "Sin edad")
+// Como un ternario, si no esta definido da "Sin edad"
+
+//
+let nombre = ""
+console.log(nombre || "Invitado")
+
+//
+let edad = 19;
+edad >= 18 && console.log("Puede entrar");
+//La primera condición se debe cumplir para que se ejecute el log
+
+//
+let alumno = {
+   nombre: "Juan",
+   edad: 20,
+   carrera: "ISC"
+}
+
+delete alumno.edad
+console.log(alumno)
+// Elimina atributos o propiedades
+
+//
+let alumno = {
+   nombre: "Juan",
+   edad: 20,
+   carrera: "ISC"
+}
+
+delete alumno.edad
+console.log(alumno)
+console.log("nombre" in alumno);
+console.log("edad" in alumno);
+// Permite verrificar propiedades de un objeto
+
+//
+const numeros = [1, 2, 3];
+const copia = [...numeros]
+
+console.log(copia)
+//Hace una copia del arreglo
+
+//
+
+const a = [1, 2]
+const b = [3, 4]
+const c = [...a, ...b]
+
+console.log(c)
+// Permite combinar arreglos
+
+//
+const productos = [
+   { nomnre: "Laptop", precio: 1500, categoria: "Electrónica" },
+   { nombre: "Celular", precio: 2900, categoria: "Telefonía" },
+   { nombre: "TV", precio: 6000, categoria: "Electróncia" },
+   { nombre: "Moto", precio: 28000, categoria: "Transporte" }
+];
+console.log(productos)
+//
+
+//
+const productos = [
+   { nomnre: "Laptop", precio: 1500, categoria: "Electrónica" },
+   { nombre: "Celular", precio: 2900, categoria: "Telefonía" },
+   { nombre: "TV", precio: 6000, categoria: "Electróncia" },
+   { nombre: "Moto", precio: 28000, categoria: "Transporte" }
+];
+
+const caros = productos.filter(p => p.precio > 10000);
+console.log(caros)
+// Ps permite filtrar 
+
+//
+const productos = [
+   { nombre: "Laptop", precio: 1500, categoria: "Electrónica" },
+   { nombre: "Celular", precio: 2900, categoria: "Telefonía" },
+   { nombre: "TV", precio: 6000, categoria: "Electróncia" },
+   { nombre: "Moto", precio: 28000, categoria: "Transporte" }
+];
+
+const caros = productos.filter(p => p.precio > 10000);
+const nombres = productos.map(p => p.nombre)
+console.log(nombres)
+console.log(caros)
+// Ps da el nombre 
+
+//
+const productos = [
+   { nombre: "Laptop", precio: 1500, categoria: "Electrónica" },
+   { nombre: "Celular", precio: 2900, categoria: "Telefonía" },
+   { nombre: "TV", precio: 6000, categoria: "Electróncia" },
+   { nombre: "Moto", precio: 28000, categoria: "Transporte" }
+];
+
+const caros = productos.filter(p => p.precio > 10000);
+const nombres = productos.map(p => p.nombre)
+const total = productos.reduce((suma, p) => {
+   return suma + p.precio;
+}, 0)
+console.log(total)
+// Retorna una suma de precios
+// El 0 es para ver en que variable iniciar 
+*/
+
+
